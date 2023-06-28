3dc6099f383e1851a59615039c50ef5fb030903b
DrZamora01 <112430390+DrZamora01@users.noreply.github.com>
2023-06-28 16:03:43 -0400

    Update base.cpp

--- mysteryhorror/base.cpp
+++ mysteryhorror/base.cpp
@@ -3,46 +3,44 @@
 
 using namespace std;
 
-void clearScreen()
-{
-    cout << string(50, '\n');
-}
-
 void displayIntroduction()
 {
-    cout << "Welcome to the Scooby doo Horror Text Adventure Game!\n";
+    cout << "Welcome to the Scooby Doo Horror Text Adventure Game!\n";
     cout << "Scooby Doo: The Mystery of the Haunted Mansion\n";
     cout << "==============================================\n";
     cout << "You are part of the Scooby Doo gang, solving mysteries together.\n";
     cout << "But Fred wants to split up the gang to solve the mystery of the haunted mansion.\n";
     cout << "You must convince the gang to stay together and solve the mystery as a team!\n\n";
     cout << "==================================\n";
-    
 }
-void playConvicefred(bool& isGangTogether);
+
+void playConvinceFred(bool& isGangTogether)
 {
     string playerInput;
 
-    bool isGangTogether = true;
-
-    while (true) {
-        cout << "What would you like to do? ";
+    while (!isGangTogether) {
+        cout << "What would you like to do? \n";
+        cout << "1. Look\n";
+        cout << "2. Talk to Shaggy\n";
+        cout << "3. Talk to Velma\n";
+        cout << "4. Talk to Daphne\n";
+        cout << "5. Talk to Fred\n";
         getline(cin, playerInput);
 
-        if (playerInput == "look") {
+        if (playerInput == "1") {
             cout << "You are standing outside the haunted mansion.\n";
             cout << "You see Shaggy, Velma, and Daphne nearby.\n";
         }
-        else if (playerInput == "talk to Shaggy") {
+        else if (playerInput == "2") {
             cout << "Shaggy: Like, I'm not sure about splitting up, man. It's dangerous!\n";
         }
-        else if (playerInput == "talk to Velma") {
+        else if (playerInput == "3") {
             cout << "Velma: Fred, we should stick together. Our teamwork is our strength!\n";
         }
-        else if (playerInput == "talk to Daphne") {
+        else if (playerInput == "4") {
             cout << "Daphne: I agree with Velma. We should work as a team to solve this mystery.\n";
         }
-        else if (playerInput == "talk to Fred") {
+        else if (playerInput == "5") {
             if (isGangTogether) {
                 cout << "Fred: I think splitting up will cover more ground. We can reunite later.\n";
                 cout << "You need to convince Fred to keep the gang together!\n";
@@ -57,50 +55,26 @@ void playConvicefred(bool& isGangTogether);
             cout << "I don't understand that command.\n";
         }
     }
-
 }
-void exampleChoices()
+
+void playGatherItems(bool& hasFlashlight, bool& hasKey)
 {
-   
+    string playerInput;
 
-    cout << "\nThe gang decides to stay together and enters the haunted mansion.\n";
-    cout << "They encounter various spooky challenges and eventually solve the mystery.\n";
-    cout << "Congratulations! The gang successfully foils the ghost's plans!\n";
-}
     while (true) {
-        cout << "What would you like to do? ";
+        cout << "What would you like to do?\n";
+        cout << "1. Look\n";
+        cout << "2. Open door\n";
+        cout << "3. Find key\n";
+        cout << "4. Find flashlight\n";
+        cout << "5. Use flashlight\n";
+        cout << "6. Use key\n";
         getline(cin, playerInput);
 
-        if (playerInput == "action") {
-            cout << "";
-        }
-        else if (playerInput == "action2") {
-            cout << "";
-        }
-        else if (playerInput == "action3") {
-            cout << "";
-        }
-        else if (playerInput == "action4") {
-            cout << "";
-            break;
-        }
-        else {
-            cout << "I don't understand that command.\n";
-        }
-    }
-}
-
-// next game play gatheritems1
-void gatheritems1(hasFlashlight, hasKey)
-{
-    while (true) {
-        cout << "What would you like to do? ";
-        getline(cin, playerInput);
-
-        if (playerInput == "look") {
+        if (playerInput == "1") {
             cout << "You are in a dark room. You can't see anything.\n";
         }
-        else if (playerInput == "open door") {
+        else if (playerInput == "2") {
             if (hasKey) {
                 cout << "You unlock the door and enter the next room.\n";
                 break;
@@ -109,15 +83,15 @@ void gatheritems1(hasFlashlight, hasKey)
                 cout << "The door is locked. You need to find a key.\n";
             }
         }
-        else if (playerInput == "find key") {
+        else if (playerInput == "3") {
             cout << "You find a key under the bed.\n";
             hasKey = true;
         }
-        else if (playerInput == "find flashlight") {
+        else if (playerInput == "4") {
             cout << "You find a flashlight on the table.\n";
             hasFlashlight = true;
         }
-        else if (playerInput == "use flashlight") {
+        else if (playerInput == "5") {
             if (hasFlashlight) {
                 cout << "You turn on the flashlight. The room is now visible.\n";
             }
@@ -125,7 +99,7 @@ void gatheritems1(hasFlashlight, hasKey)
                 cout << "You don't have a flashlight.\n";
             }
         }
-        else if (playerInput == "use key") {
+        else if (playerInput == "6") {
             cout << "You can't use the key here.\n";
         }
         else {
@@ -136,12 +110,14 @@ void gatheritems1(hasFlashlight, hasKey)
 
 int main()
 {
-    clearScreen();
     displayIntroduction();
-    exampleChoices();
-    playHauntedMansion();
-    playConvicefred(isGangTogether);
-    playGatheritem1();
+
+    bool isGangTogether = true;
+    bool hasFlashlight = false;
+    bool hasKey = false;
+
+    playConvinceFred(isGangTogether);
+    playGatherItems(hasFlashlight, hasKey);
 
     return 0;
 }
